5d672fed055ffa9b58c31ef25810c6e57603c87d
ranapk324 <ranaprathap.katepogu@gmail.com>
2026-06-14 16:05:42 +0530

    Implement delete product API

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -91,9 +91,38 @@ const updateProduct = async (req, res) => {
 
 };
 
+const deleteProduct = async (req, res) => {
+
+    try {
+
+        const product = await Product.findByIdAndDelete(req.params.id);
+
+        if (!product) {
+
+            return res.status(404).json({
+                message: "Product not found"
+            });
+
+        }
+
+        res.status(200).json({
+            message: "Product Deleted"
+        });
+
+    } catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+};
+
 module.exports = {
     createProduct,
     getProduct,
     getProductById,
-    updateProduct
+    updateProduct,
+    deleteProduct
 };
--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct , getProduct, getProductById,updateProduct} = require("../controllers/productController");
+const { createProduct , getProduct, getProductById,updateProduct,deleteProduct} = require("../controllers/productController");
 const authMiddleware = require("../middleware/authmiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 
@@ -14,4 +14,10 @@ router.post(
 router.get("/",getProduct);
 router.get("/:id",getProductById)
 router.put("/:id", authMiddleware,adminMiddleware,updateProduct)
+router.delete(
+    "/:id",
+    authMiddleware,
+    adminMiddleware,
+    deleteProduct
+);
 module.exports = router;
