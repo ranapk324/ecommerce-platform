ba55c274df45831836512abdbc829ea131d673e5
ranapk324 <ranaprathap.katepogu@gmail.com>
2026-07-15 20:46:30 +0530

    Implement admin get all orders API

--- server/src/controllers/orderController.js
+++ server/src/controllers/orderController.js
@@ -123,10 +123,31 @@ const getOrderById = async (req, res) => {
 
 };
 
+const getAllOrders = async (req, res) => {
+
+    try {
+
+        const orders = await Order.find()
+            .populate("user", "name email")
+            .populate("items.product");
+
+        res.status(200).json(orders);
+
+    } catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+};
+
 module.exports = {
 
     createOrder,
     getMyOrders,
-    getOrderById
+    getOrderById,
+    getAllOrders
 
 };
--- server/src/routes/orderRoutes.js
+++ server/src/routes/orderRoutes.js
@@ -2,15 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createOrder , getMyOrders , getOrderById} = require("../controllers/orderController");
+const { createOrder , getMyOrders , getOrderById ,getAllOrders} = require("../controllers/orderController");
 
 const authMiddleware = require("../middleware/authmiddleware");
+const adminMiddleware = require("../middleware/adminMiddleware");
 
 
 router.get(
     "/",
     authMiddleware,
     getMyOrders
+);
+
+router.get(
+    "/all",
+    authMiddleware,
+    adminMiddleware,
+    getAllOrders
 );
 
 router.get(
